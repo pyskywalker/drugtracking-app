916b544dcd00956a6e66dbe9a2b360a769ce8d96
pyskywalker <79524205+pyskywalker@users.noreply.github.com>
2021-05-26 23:26:33 +0300

    'usertype_change'

--- DTS/admin.py
+++ DTS/admin.py
@@ -7,7 +7,7 @@ from .hub_models import *
 hubmodels=[Location]
 transactionmodels=[Transaction]
 stockmodels=[Batch,Medicine]
-usermodels=[Designation,Profile]
+usermodels=[UserType,Profile]
 
 mymodels=usermodels+stockmodels+hubmodels+transactionmodels
 for model in mymodels:
