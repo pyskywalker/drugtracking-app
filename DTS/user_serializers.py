1bfbd7109c58c2e8c6fb4591ad771065b0f19aa6
pyskywalker <79524205+pyskywalker@users.noreply.github.com>
2021-05-26 22:47:20 +0300

    'edits4'

--- DTS/user_serializers.py
+++ DTS/user_serializers.py
@@ -12,16 +12,16 @@ from knox.models import AuthToken
 #     def create(self, validated_data):
 #         user = User.objects.create_user(validated_data['username'], validated_data['password'])
 #         return user
-class UserTypeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=UserType
-        fields="__all__"
-class UserSerializer(serializers.ModelSerializer):
-    usertype=UserTypeSerializer()
-    class Meta:
-        model=UserProfile
-        fields="__all__"
-class RestrictedUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=UserProfile
-        fields=['id','first_name','last_name','room','usertype','phone']
+# class UserTypeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=UserType
+#         fields="__all__"
+# class UserSerializer(serializers.ModelSerializer):
+#     usertype=Designation()
+#     class Meta:
+#         model=UserProfile
+#         fields="__all__"
+# class RestrictedUserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model=UserProfile
+#         fields=['id','first_name','last_name','room','usertype','phone']
