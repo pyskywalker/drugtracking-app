e3aeee302f6687b3fc3e8c0a1ba3330cbf50f532
pyskywalker <79524205+pyskywalker@users.noreply.github.com>
2021-05-27 09:43:03 +0300

    'new-db'

--- DTS/admin.py
+++ DTS/admin.py
@@ -7,7 +7,7 @@ from .hub_models import *
 hubmodels=[Location]
 transactionmodels=[Transaction]
 stockmodels=[Batch,Medicine]
-usermodels=[Designation,Profile]
+usermodels=[UserType,UserProfile]
 
 mymodels=usermodels+stockmodels+hubmodels+transactionmodels
 for model in mymodels:
--- DTS/user_models.py
+++ DTS/user_models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from .hub_models import *
 # Create your models here.
 
-class Designation(models.Model):
+class UserType(models.Model):
     name=models.CharField(max_length=30)
     description=models.TextField()
     date_added=models.DateTimeField(auto_now_add=True)
@@ -11,11 +11,11 @@ class Designation(models.Model):
     def __str__(self):
         return f'{self.id}:{self.name}'
 
-class Profile(models.Model):
+class UserProfile(models.Model):
     actual_user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='system_users')
     title=models.CharField(max_length=30)
     location=models.ForeignKey(Location,on_delete=models.DO_NOTHING,null=True)
-    designation=models.ForeignKey(Designation,on_delete=models.SET_NULL,null=True)
+    user_type=models.ForeignKey(UserType,on_delete=models.SET_NULL,null=True)
     organization=models.CharField(max_length=50)
     date_added=models.DateTimeField(auto_now_add=True)
     date_modified=models.DateTimeField(auto_now=True)
